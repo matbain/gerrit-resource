a6434fab04c8489c8544a25bf1aaa21e2937be14
Mark Alston <malston@pivotal.io>
2016-07-29 12:37:18 -0600

    Add some tests

---
+++ test/all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+export TMPDIR_ROOT=$(mktemp -d /tmp/gerrit-tests.XXXXXX)
+
+$(dirname $0)/image.sh
+$(dirname $0)/check.sh
+# $(dirname $0)/get.sh
+# $(dirname $0)/put.sh
+
+echo -e '\e[32mall tests passed!\e[0m'
+
+rm -rf $TMPDIR_ROOT
